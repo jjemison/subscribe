91d92c51262c850798cc216b0d9a1426a94bdff9
jjemison <jordanjemison@gmail.com>
2015-07-29 10:05:17 -0400

    remove and delete subscription

--- app/subscribe/subscribe.js
+++ app/subscribe/subscribe.js
@@ -11,23 +11,31 @@ angular.module('mySub.subscribe', ['ngRoute', 'firebase'])
 
 
 .controller('SubscribeCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-
+  // Init Firebase
   var ref = new Firebase('https://subscription-tracker.firebaseio.com/subscribe');
-
+  // get Subscribe
   $scope.subscribe = $firebaseArray(ref);
-  console.log($scope.subscribe);
+  // console.log($scope.subscribe);
 
+  // Show Add Form
   $scope.showAddForm = function() {
     $scope.addFormShow = true;
   }
+  // Show Edit Form
+  $scope.showEditForm = function(subscribe) {
+    $scope.editFormShow = true;
 
+    $scope.name = subscribe.name;
+    $scope.price = subscribe.price;
+  }
+  // Hide Form
   $scope.hide = function() {
     $scope.addFormShow = false;
     $scope.contactShow = false;
   }
-
+  //  Add Subscription 
   $scope.addFormSubmit = function() {
-    console.log('addining contact...')
+    console.log('adding contact...')
     
     // assigning values
     if($scope.name) { var name = $scope.name } else { var name = null;}
@@ -38,7 +46,7 @@ angular.module('mySub.subscribe', ['ngRoute', 'firebase'])
       price: price
     }).then(function(ref) {
       var id = ref.key();
-      console.log('added contact with ID:'+id)
+      console.log('added contact with ID:'+ id)
 
       clearFields();
 
@@ -47,10 +55,46 @@ angular.module('mySub.subscribe', ['ngRoute', 'firebase'])
       $scope.msg = "subscription added"
     });
   }
+  //  Edit Subscription 
+  $scope.editFormSubmit = function() {
+    console.log('updating subscription information')
+    
+    // get the id
+    var id = $scope.id;
+    
+    // get the record
+    var record = $scope.subscribe.$getRecord(id);
+    
+    // assigning values 
+    record.name = $scope.name;
+    record.price = $scope.price; 
 
+    // save values
+    $scope.subscribe.$save(record).then(function(ref){
+      console.log(ref.key);
+    });
+
+    clearFields();
+
+    // hide edit form
+    $scope.editFormShow = false; 
+
+    $scope.msg = "subscriber updated";
+  }
+
+  //  Clear forms 
   function clearFields() {
     $scope.name = '';
     $scope.price= '';
+  }
+
+  //remove 
+  $scope.removeSubscription = function(subscriber){
+    console.log("removing contact");
+
+    $scope.subscribe.$remove(subscriber);
+    $scope.msg = "Item has been removed";
+
   }
 
   $scope.showSubsribe = function(subscribe){
